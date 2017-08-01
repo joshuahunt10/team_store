dab5b327404b5af9475191d6c093607a45ea8d2b
joshuahunt10 <joshua.hunt88@gmail.com>
2017-08-01 14:41:57 -0500

    product displayed on homepage and details page rendering

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,16 +1,49 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: {
+        data: {
+          fullStack: []
+        }
+      }
+    }
+  }
 
   componentDidMount() {
     fetch('http://localhost:4000/api')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+      this.setState({
+        items: data
+      })
+    })
   }
   render() {
+    let itemArray = this.state.items.data.fullStack
+    console.log('logging the state of item', itemArray);
+    var topFive = itemArray.slice(0,5)
+
+    console.log(topFive);
+
     return (
       <div>
         <h1>This page lists the top 5 products by views.</h1>
+        {topFive.map((item) => {
+          return(
+            <div key={item._id}>
+              <Link to={`/products/${item._id}`} ><h2>{item.name}</h2> </Link>
+              <h4>{item.vendor}</h4>
+              <img src={item.image} />
+              <p>{item.price}</p>
+              <hr />
+            </div>
+          )
+        })}
+
       </div>
     );
   }
--- client/src/components/ProductDetails.js
+++ client/src/components/ProductDetails.js
@@ -1,58 +1,43 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 
-import React, { Component } from 'react';
 
-class View extends Component {
-
-  render() {
-    if (this.props.endpoint === 'products') {
-      let product = this.props.details
-      return (
-        <div className="card">
-          <div className='title'>
-            <h3>{product.name}</h3>
-          </div>
-          <hr>
-          <div className='image'>
-            <img src='{product.image}'>
-          <div>
-          <div className='description'>
-            <p>{product.description}</p>
-          </div>
-        </div>
-      )
-    } else {
-      <div>YOOOOOOO</div>
-    }
-  }
-}
-
-class Details extends Component {
+class ProductDetails extends Component {
   constructor(props) {
     super(props);
-    this.state= {
-      'details': {},
-      'endpoint': ''
+    this.state = {
+      item: []
     }
   }
 
   componentDidMount(){
-    let endpoint = this.props.match.params.endpoint
-    let id = this.props.match.params.id
-    let url = 'ENTER_URL_HERE' + endpoint + '/' + id
-
-
+    fetch('http://localhost:4000/api')
+    .then(res => res.json())
+    .then(data => {
+      const item = data.data.fullStack.find( (item) => {
+        return item._id === this.props.match.params.id
+      })
+      this.setState({
+        item: item
+      })
+    })
   }
 
   render() {
-    return (
-      <div>
-        <h1>{this.props.match.params.endpoint}</h1>
-        <View details ={this.state.details} endpoint={this.state.endpoint}/>
-      </div>
-    );
-  }
-}
+    console.log(this.state.item);
+    let item = this.state.item
+      return (
+        <div className="card">
+          <h1>{item.name}</h1>
+          <h3>{item.supplier}</h3>
+          <img src={item.image} />
+          <p>{item.description}</p>
+          <strong>${item.price}</strong>
 
-export default Details;
+        </div>
+      )
+    }
+  }
+
+
+export default ProductDetails
